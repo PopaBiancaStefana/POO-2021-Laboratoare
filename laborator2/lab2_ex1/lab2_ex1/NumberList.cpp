ba7e104ca3f1b94f95fbb75885253efe91c1e625
POPA Bianca-Stefana <popabianca945@yahoo.com>
2021-02-23 13:04:13 +0200

    functia Init si Print

---
+++ laborator2/lab2_ex1/lab2_ex1/NumberList.cpp
@@ -0,0 +1,11 @@
+#include "NumberList.h"
+
+void NumberList::Init()
+{
+	count = 0;
+}
+
+void NumberList::Print()
+{
+	cout <<"Numar elemente: " count << endl;
+}
---
+++ laborator2/lab2_ex1/lab2_ex1/main.cpp
@@ -0,0 +1,9 @@
+#include "NumberList.h"
+
+int mnain()
+{
+	NumberList nl;
+	nl.Init();
+	nl.Print();
+	return 0;
+}
