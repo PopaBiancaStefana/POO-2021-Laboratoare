b1822b24389011f7f7ddbeadc9fd79c130e77eec
POPA Bianca-Stefana <popabianca945@yahoo.com>
2021-03-02 13:20:33 +0200

    exercitiul 2

--- laborator3/lab3_ex1/lab3_ex1/main.cpp
+++ laborator3/lab3_ex1/lab3_ex1/main.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
-	std::cout<<Math::Add(3, 6, 7, 8);
+	std::cout << Math::Add(3, 6)<<endl;
+	std::cout << Math::Add(3, 6, 7) << endl;
+	std::cout << Math::Add(1.5, 4.2) << endl;
+	std::cout << Math::Add(9.7, 4.2, 1.7) << endl;
+	std::cout<<Math::Add(3, 6, 7, 8) << endl;
+	std::cout << Math::Mul(2, 5) << endl;
+	std::cout << Math::Mul(2, 3, 5) << endl;
+	std::cout << Math::Mul(2.3, 5.0) << endl;
+	std::cout << Math::Mul(2.3, 5.4, 1.2)<< endl;
+
 	return 0;
 }
