4f73000949a727b41823ef6e0834d8cb797e2626
POPA Bianca-Stefana <popabianca945@yahoo.com>
2021-03-02 12:05:14 +0200

    seminar3, ex1, metode

---
+++ laborator3/lab3_ex1/lab3_ex1/Math.cpp
@@ -0,0 +1,48 @@
+#include "Math.h"
+
+static int Add(int x, int y) 
+{
+	return x + y;
+}
+
+static int Add(int x, int y, int z)
+{
+	return x + y + z;
+}
+
+static int Add(double x, double y);
+{
+	return x + y;
+}
+
+static int Add(double x, double y, double z)
+{
+	return x + y + z;
+}
+
+static int Mul(int x, int y)
+{
+	return x * y;
+}
+
+static int Mul(int x, int y, int z)
+{
+	return x * y * z;
+}
+
+static int Mul(double x, double y)
+{
+	return x * y;
+}
+
+static int Mul(double x, double y, double z)
+{
+	return x * y * z;
+}
+
+static int Add(int count x, ...); // sums up a list of integers
+
+static char* Add(const char* x, const char* y)
+{
+
+}
---
+++ laborator3/lab3_ex1/lab3_ex1/main.cpp
@@ -0,0 +1,6 @@
+#include "Math.h"
+
+int main()
+{
+
+}
